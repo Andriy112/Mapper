01e3940a038a464155891cefb9be2ad76a281dc1
Andriy112 <86891549+Andriy112@users.noreply.github.com>
2022-11-05 19:10:42 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Mapper;
+
+string name = "Tom";
+int age = 18;
+dynamic variable1 = new { name,age };
+MapperBase mapper = new Mapper.Mapper();
+Person []people = mapper.ConvertArray<dynamic,Person>(new[] { variable1},parser
+    => parser.FromPropertyManualy((dynamic from, Person to) => 
+    {
+    to.Age = variable1.age;
+    to.Name = variable1.name;
+    }));
+Console.WriteLine("Hello mapper {0}!", people[0].Name);
